94e1cad12fc4700fd6dc0725cc9f6361918a9801
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-09-26 22:17:05 +0300

    edit

--- create_table.py
+++ create_table.py
@@ -9,9 +9,24 @@ CREATE TABLE IF NOT EXISTS groupname (
 );
 
 """
+create_table_facultet = """
+CREATE TABLE IF NOT EXISTS facultet (
+  id SERIAL PRIMARY KEY,
+  facultet VARCHAR(50)
+);
 
+"""
+create_table_course = """
+CREATE TABLE IF NOT EXISTS course (
+  id SERIAL PRIMARY KEY,
+  coursename VARCHAR(20)
+);
+
+"""
 if __name__ == '__main__':
     with connection() as conn:
         c = conn.cursor()
-        c.execute(create_table_group)
+        # c.execute(create_table_group)
+        # c.execute(create_table_facultet)
+        c.execute(create_table_course)
         c.close()
