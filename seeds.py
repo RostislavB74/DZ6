b8209c51f6153629f426c6ac560c2d733ab44eab
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-09-28 21:50:33 +0300

    edit

--- seeds.py
+++ seeds.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from faker import Faker
 from random import randint, choice
 import sqlite3
 from pprint import pprint
+from tqdm import tqdm
 
 items = [
     "Вища математика",
@@ -30,7 +31,7 @@ groups = ["АКФ 101", "АКФ 201", "АКФ 301", "АКФ 401", "АКФ 501",
           "АКФ 102", "АКФ 202", "АКФ 302", "АКФ 402", "АКФ 502",
           "АКФ 103", "АКФ 203", "АКФ 303", "АКФ 403", "АКФ 503"
           ]
-#teachers = ["Тарасенко Т.В.", "Богдан С.Ю.", "Агеєв С.Є.", "Орденов С.С."]
+# teachers = ["Тарасенко Т.В.", "Богдан С.Ю.", "Агеєв С.Є.", "Орденов С.С."]
 NUMBER_TEACHERS = 15
 NUMBER_STUDENTS = 300
 fake = Faker()
@@ -61,11 +62,37 @@ def seed_groups():
     sql = "INSERT INTO groups (name) VALUES(?);"
     cur.executemany(sql, zip(groups,))
 
+
+def seed_grades():
+    start_date = datetime.strptime("2022-09-01", "%Y-%m-%d")
+    end_date = datetime.strptime("2022-12-25", "%Y-%m-%d")
+    sql = "INSERT INTO grades (items_id, students_id, grade, date_of) VALUES(?, ?, ?, ?);"
+
+    def get_list_date(start: date, end: date):
+        result = []
+        current_date = start
+        while current_date < end:
+            if current_date.isoweekday() < 6:
+                result.append(current_date)
+                current_date += timedelta(1)
+        return result
+
+    list_dates = get_list_date(start_date, end_date)
+    grades = []
+    for day in tqdm(list_dates):
+        random_items = randint(1, len(items))
+        random_student = [randint(1, len(NUMBER_STUDENTS)) for _ in range(5)]
+        for student in tqdm(random_student):
+            grades.append((random_items, student, randint(1, 12), day.date()))
+    cur.executemany(sql, grades)
+
+
 if __name__ == '__main__':
     try:
         seed_teachers()
         seed_items()
         seed_groups()
+        seed_grades()
         seed_students()
         connect.commit()
     except sqlite3.Error as error:
